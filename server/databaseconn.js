43f6f2ee5dc5ec4c7b9f8a1f5652cc6a12754d53
Imran Ahmed <imranhahmed0987@gmail.com>
2024-12-15 17:59:31 -0500

    cleaned databseconn.js

--- server/databaseconn.js
+++ server/databaseconn.js
@@ -23,28 +23,4 @@ const getConnection =()  =>
 
 
 
-
-// Create database using these 2 lines:
-// const createDatabaseQuery = 'create database if not exists mp3database';
-
-// connectionPool.query(createDatabaseQuery);
-
-//creating song info table
-//Needs:
-//unique id, song title, artist,
-// const createSongInfo = `create table if not exists songInfo (id int AUTO_INCREMENT PRIMARY KEY, song_title text, artist text, filename text);`;
-// connectionPool.query(createSongInfo);
-
-//adding dummy entry to test out server connectivity
-// const initialdataAddition = `insert into songInfo (song_title,artist,filename) values ('Mortal Man','Kendrick Lamar','Users/Music/Mortal_man.mp3')`;
-// connection.query(initialdataAddition, (error, results)=>
-// {
-//     if (error)
-//     {
-//         console.error("Issue adding info",error);
-//         return;
-//     }
-//     console.log("Information successfully added")
-// });
-
 export {connectionPool, getConnection};    
