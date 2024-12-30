4025d4235ffa770372e44ffc8ba7eedd438ff0b4
Imran Ahmed <imranhahmed0987@gmail.com>
2024-12-29 22:55:13 -0500

    frontend changes

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,6 @@ import SongsList from './SongsList'
 function App(){
     return(
         <div>
-            <h1>MP3 Uploader</h1>
             <SongsList />
         </div>
     )
--- client/src/SongsList.js
+++ client/src/SongsList.js
@@ -1,6 +1,5 @@
-//Handling simple get request to import in App.js
 import axios from 'axios';
-import React,{useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -8,82 +7,54 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import rootShouldForwardProp from '@mui/material/styles/rootShouldForwardProp';
 
-function useFetchSongs()
-{
+function useFetchSongs() {
     const [songs, setSongs] = useState([]);
 
-
-
-    useEffect(() =>
-    {
-        axios.get('http://localhost:3000/songs').then(response =>{
-            setSongs(response.data);
-        })
-        .catch(error =>
-        {
-            console.error("Error fetching songs",error);
-        });
-    },[]);
+    useEffect(() => {
+        axios.get('http://localhost:3000/songs')
+            .then((response) => {
+                setSongs(response.data);
+            })
+            .catch((error) => {
+                console.error('Error fetching songs', error);
+            });
+    }, []);
 
     return songs;
 }
 
-const SongsList =() =>
-{
+const SongsList = () => {
     const songs = useFetchSongs();
-    return ( 
-        <TableContainer component={songsList}>
-            <Table sx={} aria-label='song table'>
+
+    return (
+        <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="song table">
                 <TableHead>
                     <TableRow>
-                        <TableCell align='right'>Title</TableCell>
-                        <TableCell align='right'>Artist</TableCell>
+                        <TableCell align='right'>ID</TableCell>
+                        <TableCell align="right">Title</TableCell>
+                        <TableCell align="right">Filename</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {songs.map((song) =>
-                    {
-                        <TableRow 
-                        key={song.name}
-                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-
-                            
+                    {songs.map((song) => (
+                        <TableRow
+                            key={song.title}
+                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        >
+                            <TableCell component="th" scope="song">
+                                {song.id}
+                            </TableCell>
+                            <TableCell align="left">{song.song_title}</TableCell>
+                            <TableCell align="right">{song.artist}</TableCell>
+                            <TableCell align="right">{song.filename}</TableCell>
                         </TableRow>
-
-                    })}
+                    ))}
                 </TableBody>
             </Table>
         </TableContainer>
-    
-
-
-    
-    
-    
-        //     <div className='table'> 
-    //     <table>
-    //             <tr>
-    //                 <th>Title</th>
-    //                 <th>Artist</th>
-    //                 <th>File</th>
-    //             </tr>
-    //             {songs.map((song) =>
-    //             {
-    //                 return(
-    //                     <tr key={song.id}>
-    //                         <td>{song.song_title}</td>
-    //                         <td>{song.artist} </td>
-    //                     </tr>
-    //                 )
-    //             })}
-    //         </table> 
-              
-        
-    // </div>
-)
+    );
 };
-
 
 export default SongsList;
